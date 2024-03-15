34688b87b14b08c51de30555d1a2897c41ec2292
unknown <hsnc24021407@hanwha.com>
2024-03-15 16:26:00 +0900

    스터디 - 대시보드 기능 추가

--- common/views.py
+++ common/views.py
@@ -20,36 +20,82 @@ def main(request):
     """
     Dashboard 출력
     """
+    symbol = request.GET.get('symbol', 'TSLA')  # 페이지
+
+    stock_data = ResultData.objects.filter(Q(func_name='stock_data_call') & Q(key_name=symbol)).order_by('-receipt_date').first()
     exch_data = ResultData.objects.filter(Q(func_name='exch_data_call') & Q(key_name='KRW2USD')).order_by('-receipt_date').first()
     index_data = ResultData.objects.filter(Q(func_name='index_data_call')).order_by('-receipt_date').first()
     crypto_data = ResultData.objects.filter(Q(func_name='crypto_data_call')).order_by('-receipt_date').first()
     tech_data = ResultData.objects.filter(Q(func_name='tech_data_call')).order_by('-receipt_date').first()
+    hist_data = ResultData.objects.filter(Q(func_name='stock_hist_data_call') & Q(key_name=symbol)).order_by('-receipt_date').first()
+    earn_data = ResultData.objects.filter(Q(func_name='stock_earning_data_call') & Q(key_name=symbol)).order_by('-receipt_date').first()
 
+    stock_dict_data = json.loads(stock_data.result_data)
     exch_json_data = json.loads(exch_data.result_data)
     index_json_data = json.loads(index_data.result_data)
     crypto_json_data = json.loads(crypto_data.result_data)
     tech_list = json.loads(tech_data.result_data).get('tech_list')
+    hist_list = json.loads(hist_data.result_data).get('hist_list')
+    earn_label_list = json.loads(earn_data.result_data).get('label_list')
+    earn_act_data_list = json.loads(earn_data.result_data).get('act_data_list')
+    earn_est_data_list = json.loads(earn_data.result_data).get('est_data_list')
 
-    main_data = {
-        'exch_rate': exch_json_data.get('Exch Rate'),
-        'exch_rate_change': exch_json_data.get('Exch Rate Change'),
-        'exch_rate_change_percent': exch_json_data.get('Exch Rate Change Percent'),
-        'exch_rate_color': colorSelection(exch_json_data.get('Exch Rate Change')),
-        'snp500': index_json_data.get('S&P 500'),
-        'snp500_change': index_json_data.get('S&P 500 Change'),
-        'snp500_change_percent': index_json_data.get('S&P 500 Change Percent'),
-        'snp500_color': colorSelection(index_json_data.get('S&P 500 Change')),
-        'nasdaq': index_json_data.get('NASDAQ Composite'),
-        'nasdaq_change': index_json_data.get('NASDAQ Composite Change'),
-        'nasdaq_change_percent': index_json_data.get('NASDAQ Composite Change Percent'),
-        'nasdaq_color': colorSelection(index_json_data.get('NASDAQ Composite Change')),
-        'bitcoin': crypto_json_data.get('Bitcoin USD'),
-        'bitcoin_change': crypto_json_data.get('Bitcoin USD Change'),
-        'bitcoin_change_percent': crypto_json_data.get('Bitcoin USD Change Percent'),
-        'bitcoin_color': colorSelection(crypto_json_data.get('Bitcoin USD Change'))
-    }
+    hist_label_list = list()
+    hist_data_list = list()
 
-    return render(request, 'common/main.html', {'main_data': main_data, 'tech_list': tech_list})
+    for index, hist_data in enumerate(hist_list):
+        hist_label_list.append(hist_data.get('date'))
+        hist_data_list.append(hist_data.get('price'))
+
+    hist_label_list.reverse()
+    hist_data_list.reverse()
+
+    main_data = dict()
+
+    main_data['ticker'] = stock_dict_data.get('Ticker')
+    main_data['ticker_name'] = stock_dict_data.get('Ticker Name')
+    main_data['per'] = stock_dict_data.get('P/E')
+    main_data['forward_per'] = stock_dict_data.get('Forward P/E')
+    main_data['eps_ttm'] = stock_dict_data.get('EPS (ttm)')
+    main_data['market_cap'] = stock_dict_data.get('Market Cap')
+    main_data['earnings'] = stock_dict_data.get('Earnings')
+    main_data['eps_ttm'] = stock_dict_data.get('EPS(ttm)')
+    main_data['eps_next_yr'] = stock_dict_data.get('EPS next Y')
+
+    main_data['peg'] = stock_dict_data.get('PEG')
+    main_data['psr'] = stock_dict_data.get('P/S')
+    main_data['pbr'] = stock_dict_data.get('P/B')
+    main_data['roe'] = stock_dict_data.get('ROE')
+    main_data['gr_margin'] = stock_dict_data.get('Gross Margin')
+    main_data['op_margin'] = stock_dict_data.get('Oper.Margin')
+    main_data['pf_margin'] = stock_dict_data.get('Profit Margin')
+    main_data['roi'] = stock_dict_data.get('ROI')
+    main_data['short_float'] = stock_dict_data.get('Short Float')
+    main_data['rsi14'] = stock_dict_data.get('RSI(14)')
+    main_data['beta'] = stock_dict_data.get('Beta')
+    main_data['pt'] = stock_dict_data.get('Target Price')
+
+
+
+
+    main_data['exch_rate'] = exch_json_data.get('Exch Rate')
+    main_data['exch_rate_change'] = exch_json_data.get('Exch Rate Change')
+    main_data['exch_rate_change_percent'] = exch_json_data.get('Exch Rate Change Percent')
+    main_data['exch_rate_color'] = colorSelection(exch_json_data.get('Exch Rate Change')),
+    main_data['snp500'] = index_json_data.get('S&P 500')
+    main_data['snp500_change'] = index_json_data.get('S&P 500 Change')
+    main_data['snp500_change_percent'] = index_json_data.get('S&P 500 Change Percent')
+    main_data['snp500_color'] = colorSelection(index_json_data.get('S&P 500 Change')),
+    main_data['nasdaq'] = index_json_data.get('NASDAQ Composite')
+    main_data['nasdaq_change'] = index_json_data.get('NASDAQ Composite Change')
+    main_data['nasdaq_change_percent'] = index_json_data.get('NASDAQ Composite Change Percent')
+    main_data['nasdaq_color'] = colorSelection(index_json_data.get('NASDAQ Composite Change')),
+    main_data['bitcoin'] = crypto_json_data.get('Bitcoin USD')
+    main_data['bitcoin_change'] = crypto_json_data.get('Bitcoin USD Change')
+    main_data['bitcoin_change_percent'] = crypto_json_data.get('Bitcoin USD Change Percent')
+    main_data['bitcoin_color'] = colorSelection(crypto_json_data.get('Bitcoin USD Change'))
+
+    return render(request, 'common/main.html', {'symbol': symbol, 'main_data': main_data, 'tech_list': tech_list, 'hist_label_list': hist_label_list, 'hist_data_list': hist_data_list, 'earn_label_list': earn_label_list, 'earn_act_data_list': earn_act_data_list, 'earn_est_data_list': earn_est_data_list})
 
 def colorSelection(data):
     if data[0:1] == '+':
--- interface/views/crawling_views.py
+++ interface/views/crawling_views.py
@@ -2,6 +2,7 @@ import os
 import sys
 import urllib3
 import json
+from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -21,44 +22,56 @@ def main(request):
 
             user_agent = resultDataForm.user_agent
             target_url = resultDataForm.target_url
+            key_name = resultDataForm.key_name
 
             os.environ['WDM_SSL_VERIFY'] = '0'
 
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-            # Chrome 브라우저 열기
-            if not user_agent:
-                options = webdriver.ChromeOptions()
-            else:
-                options = Options()
-                options.add_argument('user-agent=' + user_agent)
-            options.add_argument('headless')
+            keys = key_name.split(",")
 
-            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+            for key in keys:
+                # Chrome 브라우저 열기
+                if not user_agent:
+                    options = webdriver.ChromeOptions()
+                else:
+                    options = Options()
+                    options.add_argument('user-agent=' + user_agent)
+                options.add_argument('log-level=3')
+                options.add_argument('headless')
 
-            # 페이지 연결시키기
-            driver.get(target_url)
+                driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+                # URL 파라미터 삽입
+                target_rep_url = target_url.replace('{key}', key)
 
-            # BeautifulSoup 객체 생성
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
+                # 페이지 연결시키기
+                driver.get(target_rep_url)
 
-            base_data = resultDataForm.base_data
-            base_data = base_data.replace("'", "\"")
+                # BeautifulSoup 객체 생성
+                html = driver.page_source
+                soup = BeautifulSoup(html, 'html.parser')
 
-            if not base_data:
-                mapData = dict()
-            else:
-                mapData = json.loads(base_data)
+                base_data = resultDataForm.base_data
+                base_data = base_data.replace("'", "\"")
 
-            getattr(sys.modules[__name__], resultDataForm.func_name)(mapData, soup)
+                if not base_data:
+                    mapData = dict()
+                else:
+                    mapData = json.loads(base_data)
 
-            # 웹 페이지 종료
-            driver.quit()
+                getattr(sys.modules[__name__], resultDataForm.func_name)(mapData, soup)
 
-            resultDataForm.result_data = json.dumps(mapData)
-            resultDataForm.receipt_date = timezone.now()
-            resultDataForm.save()
+                # 웹 페이지 종료
+                driver.quit()
+
+                resultDataForm.id = None
+                resultDataForm.target_url = target_rep_url
+                resultDataForm.key_name = key
+                resultDataForm.result_data = json.dumps(mapData)
+                resultDataForm.receipt_date = timezone.now()
+                resultDataForm.save()
+
+                sleep(1)
 
             return render(request, 'interface/crawling_result.html', {'result': resultDataForm})
     else:
@@ -66,9 +79,9 @@ def main(request):
     context = {'form': form}
     return render(request, 'interface/crawling_main.html', context)
 
-def finviz_data_call(mapData, soup):
+def stock_data_call(mapData, soup):
     mapData["Ticker"] = soup.find('div', attrs={"class": "quote-header_ticker-wrapper"}).h1.text
-    mapData["Ticker Name"] = soup.find('div', attrs={"class": "quote-header_ticker-wrapper"}).h2.text.strip()
+    mapData["Ticker_Name"] = soup.find('div', attrs={"class": "quote-header_ticker-wrapper"}).h2.text.strip()
 
     table = soup.find('table', {"class": "js-snapshot-table snapshot-table2 screener_snapshot-table-body"})
     trs = table.tbody.find_all('tr')
@@ -96,6 +109,53 @@ def finviz_data_call(mapData, soup):
         rating_list.append(rating_dict)
 
     mapData['Rating List'] = rating_list
+
+def stock_hist_data_call(mapData, soup):
+    table = soup.find('table', attrs={"data-test": "historical-prices"})
+    trs = table.find_all('tr')
+    result_list = list()
+    trs_len = len(trs)
+    for index, tr in enumerate(trs):
+        if index > 0:
+            tds = tr.find_all('td')
+            if len(tds) == 7:
+                result_data = dict()
+                result_data['no'] = trs_len - index
+                result_data['date'] = tr.find_all('td')[0].text.strip()
+                result_data['price'] = tr.find_all('td')[4].text.strip()
+                result_data['volume'] = tr.find_all('td')[5].text.strip()
+                result_list.append(result_data)
+
+    mapData['hist_list'] = result_list
+
+def stock_earning_data_call(mapData, soup):
+    section = soup.find('section', attrs={"data-yaft-module": "tdv2-applet-AnalystLeafPage"})
+    ths = section.find_all('table')[2].find_all('th')
+    trs = section.find_all('table')[2].find_all('tr')
+    label_list = list()
+    est_data_list = list()
+    act_data_list = list()
+    for index, th in enumerate(ths):
+        if index > 0:
+            label_list.append(th.text.strip())
+
+    for index, tr in enumerate(trs):
+        if index == 1:
+            est_tds = tr.find_all('td')
+            est_data_list.append(est_tds[1].text.strip())
+            est_data_list.append(est_tds[2].text.strip())
+            est_data_list.append(est_tds[3].text.strip())
+            est_data_list.append(est_tds[4].text.strip())
+        elif index == 2:
+            act_tds = tr.find_all('td')
+            act_data_list.append(act_tds[1].text.strip())
+            act_data_list.append(act_tds[2].text.strip())
+            act_data_list.append(act_tds[3].text.strip())
+            act_data_list.append(act_tds[4].text.strip())
+
+    mapData['label_list'] = label_list
+    mapData['est_data_list'] = est_data_list
+    mapData['act_data_list'] = act_data_list
 
 def exch_data_call(mapData, soup):
     div = soup.find('div', attrs={"data-test": "instrument-header-details"})
